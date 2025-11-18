ffad57d267e4b0dd39dd38055ab1e424fc2251bf
RodoGaspar <gasparianprds@gmail.com>
2025-11-18 10:03:45 -0300

    fixing bugs in api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: process.env.MEDICAL_BOOKING_API_URL, // backend base URL
+    baseURL: process.env.NEXT_PUBLIC_API_URL, // backend base URL
 });
